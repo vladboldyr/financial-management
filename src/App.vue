bc90265cdcfc790806d0f91cc0f89363c2c9eded
vladboldyr <vladiclav.bolduryca@gmail.com>
2020-03-09 19:46:44 +0600

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <template>
   <div id="app">
-    <router-view/>
+    <component :is="layout">
+      <router-view/>
+    </component>
   </div>
 </template>
 
-<style>
+<script>
+import EmtyLayout from '@/layouts/EmptyLayout'
+import MainLayout from '@/layouts/MainLayout'
+export default {
+  computed: {
+    layout() {
+      console.log(this.$route.meta.layout);
+      return 'empty-layout';
+      //return (this.$route.meta.layout || 'empty') + '-layout';
+    }
+  },
+  components: {
+    EmtyLayout, MainLayout
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/index.css';
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,33 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('./../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('./../views/Categories.vue')
   }
-];
+]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
